f512c613db6dc354b30155848a83329e31957dc1
Luzin7 <lvictor.dutra@gmail.com>
2023-02-04 14:36:48 -0300

    Update App.jsx

--- 005-creating-a-modal/src/App.jsx
+++ 005-creating-a-modal/src/App.jsx
@@ -1,14 +1,30 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import Modal from "./components/modal";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  // using boolean to control modal opening (start FALSE)
+  const [toggleModal, setToggleModal] = useState(false);
   return (
     <div className="App">
-      <h1>Luan Victor</h1>
-      <button>Open Modal</button>
+      <h1>Creating a modal!</h1>
+      <p>Click below to open the modal.</p>
+      <button onClick={() => setToggleModal(true)}>Open Modal</button>
+      {/* using modal and add content */}
+      <Modal isOpen={toggleModal}>
+        <div className="modal__button">
+          {/* content that will be inputed in children */}
+          {/* closing modal with button */}
+          <button onClick={() => setToggleModal(false)}>X</button>
+        </div>
+        <h1>Here's the modal!</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos,
+          voluptates perspiciatis! Adipisci distinctio dolorum in impedit
+          delectus nulla ipsum soluta fugiat, saepe deserunt sapiente
+          praesentium perspiciatis explicabo perferendis porro totam?
+        </p>
+      </Modal>
     </div>
   );
 }
